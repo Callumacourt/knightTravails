91eb139555837b3d8179fa64b30157764b5e0e80
Callum <callumacourtt@gmail.com>
2024-02-22 13:32:22 +0000

    Fixed coordinate system

--- src/dom.js
+++ src/dom.js
@@ -2,12 +2,12 @@ export default function dom(chessboardContainer) {
   const chessboard = document.createElement('div');
   chessboard.classList.add('chessboard');
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 7; i >= 0; i--) {
     for (let j = 0; j < 8; j++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
-      cell.dataset.row = i;
-      cell.dataset.col = j;
+      cell.dataset.row = i + 1;
+      cell.dataset.col = j + 1;
 
       if ((i + j) % 2 === 0) {
         cell.classList.add('light');
